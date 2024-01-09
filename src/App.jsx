b79f4317a8eeeb9643fc673dc2d495f9ec4a6f3a
endulum <71853916+endulum@users.noreply.github.com>
2024-01-09 10:48:04 -0800

    moved LoginForm into a generic JSONForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import axios from 'axios';
 import useLocalStorage from './custom-hooks/useLocalStorage';
-// import './styles/main.css';
+import JSONForm from './components/JSONForm';
+import './styles/style.css';
 
 export default function App() {
   const [token, setToken] = useLocalStorage('token', null);
@@ -25,62 +26,23 @@ export default function App() {
       ) : (
         <div>
           <p>You&apos;re not logged in.</p>
-          <LoginForm onSuccess={setToken} />
+          <JSONForm
+            onSuccess={setToken}
+            method="POST"
+            endpoint="login"
+          >
+            <label htmlFor="userName">
+              <span>User Name:</span>
+              <input type="text" id="userName" />
+            </label>
+
+            <label htmlFor="password">
+              <span>Password:</span>
+              <input type="password" id="password" />
+            </label>
+          </JSONForm>
         </div>
       )}
     </div>
-  );
-}
-
-function LoginForm({ onSuccess }) {
-  const [form, setForm] = useState({
-    userName: '',
-    password: '',
-  });
-  const [errors, setErrors] = useState([]);
-
-  function handleChange(e) {
-    setForm({
-      ...form,
-      [e.target.id]: e.target.value,
-    });
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    console.log(form);
-    try {
-      const response = await axios({
-        method: 'POST',
-        url: 'http://localhost:3000/api/login',
-        data: form,
-      });
-      console.log(response.data);
-      if (response.data.accessToken) onSuccess(response.data);
-      else setErrors(response.data.errors.map((error) => error.msg));
-    } catch (err) {
-      console.error(err);
-      setErrors([err.message]);
-    }
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      {errors.length > 0 && (
-        <div>
-          <p>Errors with your submission:</p>
-          <ul>
-            {errors.map((error, index) => <li key={index}>{error}</li>)}
-          </ul>
-        </div>
-      )}
-      <label htmlFor="userName">User Name:</label>
-      <input type="text" id="userName" onChange={handleChange} />
-
-      <label htmlFor="password">Password:</label>
-      <input type="password" id="password" onChange={handleChange} />
-
-      <button type="submit">Log In</button>
-    </form>
   );
 }
